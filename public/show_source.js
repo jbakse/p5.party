63931a76a73aae2f1a398c87bbfc23b549c06356
Justin Bakse <justin@tsfim.com>
2020-06-10 16:27:02 -0400

    clean up examples styles

--- public/show_source.js
+++
@@ -1,7 +0,0 @@
-fetch("./index.js")
-  .then((r) => r.text())
-  .then((t) => {
-    const div = document.getElementById("source");
-    div.innerHTML = `<pre><code>${t}</code></pre>`;
-    hljs.initHighlighting();
-  });
