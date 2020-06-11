5fb688455a68264a2eaab8f4206f88618a52203f
Isabel Anguera <isabelanguera2@gmail.com>
2020-06-11 10:21:25 -0400

    Added 'team' wins

--- public/examples/tictactoe/index.js
+++ public/examples/tictactoe/index.js
@@ -138,51 +138,61 @@ function showOutcome() {
   stroke(30);
   strokeWeight(10);
   let gameIsWon = false;
+  let blueWins;
+  let yellowWins;
 
   // top row
   if (checkCombo(0, 1, 2)) {
+    blueWins = shared.boardState[0] === 1 ? true : false;
     line(40, 75, 410, 75);
     gameIsWon = true;
   }
 
   // middle row
   if (checkCombo(3, 4, 5)) {
+    blueWins = shared.boardState[3] === 1 ? true : false;
     line(40, 225, 410, 225);
     gameIsWon = true;
   }
 
   // bottom row
   if (checkCombo(6, 7, 8)) {
+    blueWins = shared.boardState[6] === 1 ? true : false;
     line(40, 375, 410, 375);
     gameIsWon = true;
   }
 
   // left column
   if (checkCombo(0, 3, 6)) {
+    blueWins = shared.boardState[0] === 1 ? true : false;
     line(75, 40, 75, 410);
     gameIsWon = true;
   }
 
   // middle column
   if (checkCombo(1, 4, 7)) {
+    blueWins = shared.boardState[1] === 1 ? true : false;
     line(225, 40, 225, 410);
     gameIsWon = true;
   }
 
   // right column
   if (checkCombo(2, 5, 8)) {
+    blueWins = shared.boardState[2] === 1 ? true : false;
     line(375, 40, 375, 410);
     gameIsWon = true;
   }
 
   // diagonal \
   if (checkCombo(0, 4, 8)) {
+    blueWins = shared.boardState[0] === 1 ? true : false;
     line(40, 40, 410, 410);
     gameIsWon = true;
   }
 
   // diagonal /
   if (checkCombo(2, 4, 6)) {
+    blueWins = shared.boardState[2] === 1 ? true : false;
     line(40, 410, 410, 40);
     gameIsWon = true;
   }
@@ -204,4 +214,22 @@ function showOutcome() {
   }
   pop();
   if (gameIsWon) shared.currentTurn = "nobody";
+
+  if (blueWins && gameIsWon) {
+    push();
+    fill("white");
+    noStroke();
+    textSize(22);
+    textFont("Gill Sans");
+    text("Blue Team Wins!", 155, 482);
+    pop();
+  } else if (blueWins === false && gameIsWon) {
+    push();
+    fill("white");
+    noStroke();
+    textSize(22);
+    textFont("Gill Sans");
+    text("Yellow Team Wins!", 145, 482);
+    pop();
+  }
 }
