70d71948428d155baefb3519e16a17518f87fe0c
Justin Bakse <justin@tsfim.com>
2020-06-30 15:57:05 -0400

    clean up code on d12 demo

--- public/examples/d12/index.js
+++ public/examples/d12/index.js
@@ -1,33 +1,35 @@
 const SCALE = 8;
 const TILE_SIZE = 8;
-const VIEW_WIDTH = 16;
-const VIEW_HEIGHT = 8;
+const VIEW_WIDTH = 12;
+const VIEW_HEIGHT = 12;
 
-let mode = "move";
-
+// shared
 let me;
 let players;
-let shared;
 
-const images = [];
+// local state
 const camera = { x: 0, y: 0 };
+let mode = "move";
 const localPlayerData = new WeakMap();
 
+// message
+let startMessageOnRelease = false;
 let message_input;
 let messageTimeout;
 
+// resources
+const images = [];
 let font;
-
 let p8file;
-let gfx;
 let map;
 
 function preload() {
+  // get shared data
   partyConnect("wss://deepstream-server-1.herokuapp.com", "d12", "main1");
-  shared = partyLoadShared("shared");
   me = partyLoadMyShared();
   players = partyLoadParticipantShareds();
-  images.d12_1205 = loadImage("./images/d12_1205.png");
+
+  // load resources
   images.player = loadImage("./images/player.png");
   font = loadFont("pixeldroidConsoleRegular/pixeldroidConsoleRegular.otf");
   p8file = loadStrings("p8/test.p8");
@@ -35,19 +37,16 @@ function preload() {
 
 function setup() {
   createCanvas(VIEW_WIDTH * TILE_SIZE * SCALE, VIEW_HEIGHT * TILE_SIZE * SCALE);
-  noStroke();
-  me.row = 5;
-  me.col = 5;
+
+  me.row = 1;
+  me.col = 1;
   moveCamera(me.row * TILE_SIZE, me.col * TILE_SIZE);
-  gfx = gfxFromP8(p8file);
+
+  const gfx = gfxFromP8(p8file);
   map = mapFromP8(p8file, gfx);
 }
 
-function mousePressed(e) {}
-
 function draw() {
-  background(0);
-
   // init local player data for new players
   for (const p of players) {
     if (!localPlayerData.has(p)) {
@@ -58,67 +57,85 @@ function draw() {
     }
   }
 
+  // step + draw
+  step();
+  drawGame();
+}
+
+////////////////////////////////////////////////////////
+// GAME LOGIC
+
+function step() {
   // move players
   for (const p of players) {
     const localP = localPlayerData.get(p);
-    movePoint(
-      localP,
-      {
-        x: p.col * TILE_SIZE,
-        y: p.row * TILE_SIZE,
-      },
-      1
-    );
+    const goalP = { x: p.col * TILE_SIZE, y: p.row * TILE_SIZE };
+    localP.flip = localP.x > goalP.p;
+    movePoint(localP, goalP, 1);
   }
 
   // move camera
   const localMe = localPlayerData.get(me);
   moveCamera(localMe.x, localMe.y);
-
-  // set camera
-  push();
-  {
-    scale(8);
-    translate(-camera.x, -camera.y);
-    translate(
-      (VIEW_WIDTH - 1) * 0.5 * TILE_SIZE,
-      (VIEW_HEIGHT - 1) * 0.5 * TILE_SIZE
-    );
-
-    drawView();
-  }
-  pop();
-
-  noSmooth();
 }
 
-function drawView() {
-  noSmooth();
-  push();
+function moveCamera(x, y, max) {
+  movePoint(camera, { x, y }, max);
+}
 
-  fill("black");
+function movePoint(p1, p2, max = Infinity) {
+  const dX = constrain(p2.x - p1.x, -max, max);
+  const dY = constrain(p2.y - p1.y, -max, max);
+  p1.x += dX;
+  p1.y += dY;
+}
+
+////////////////////////////////////////////////////////
+// DRAWING
+
+function drawGame() {
+  background(0);
+
+  // set camera transform
+  scale(8);
+  translate(-camera.x, -camera.y);
+  translate(
+    (VIEW_WIDTH - 1) * 0.5 * TILE_SIZE,
+    (VIEW_HEIGHT - 1) * 0.5 * TILE_SIZE
+  );
+
+  // set draw modes
+  noSmooth();
+
+  // draw map
+  fill("gray");
   rect(0, 0, map.width, map.height);
   image(map, 0, 0);
 
+  // draw players
   for (const p of players) {
     const localP = localPlayerData.get(p);
     image(images.player, localP.x, localP.y, TILE_SIZE, TILE_SIZE);
-    if (p.message) {
-      textFont(font, 16);
-      textAlign(CENTER, BOTTOM);
-      fill("black");
-      rect(
-        localP.x - textWidth(p.message) * 0.5 + 4,
-        localP.y - 8,
-        textWidth(p.message) + 1,
-        8
-      );
-      fill("white");
-      text(p.message, localP.x + 4, localP.y);
+    if (localP.flip) {
+      image(images.player, localP.x, localP.y, TILE_SIZE, TILE_SIZE);
     }
+    if (p.message) drawMessage(p.message, localP.x, localP.y);
   }
+}
+
+function drawMessage(s, x, y) {
+  push();
+  textFont(font, 16);
+  textAlign(CENTER, BOTTOM);
+  fill("black");
+  rect(x - textWidth(s) * 0.5 + 4, y - 8, textWidth(s) + 1, 8);
+  fill("white");
+  text(s, x + 4, y);
   pop();
 }
+
+////////////////////////////////////////////////////////
+// INPUT
 
 function keyPressed() {
   if (mode === "move") {
@@ -134,63 +151,59 @@ function keyPressed() {
     if (keyCode === DOWN_ARROW || key === "s") {
       me.row += 1;
     }
-
     if (key === " " || keyCode === RETURN) {
-      showInputOnRelease = true;
+      startMessageOnRelease = true;
     }
   }
 
   if (mode === "message") {
-    if (keyCode === RETURN) {
-      me.message = message_input.value();
-      clearTimeout(messageTimeout);
-      messageTimeout = setTimeout(() => {
-        me.message = undefined;
-      }, 5000);
-
-      message_input.remove();
-      mode = "move";
-    }
-    if (keyCode === ESCAPE) {
-      message_input.remove();
-      mode = "move";
-    }
+    if (keyCode === RETURN) sendMessage();
+    if (keyCode === ESCAPE) cancelMessage();
   }
 }
 
-let showInputOnRelease = false;
 function keyReleased() {
-  if (showInputOnRelease) {
-    mode = "message";
-    message_input = createInput("");
-    message_input.id("message-input");
-    message_input.attribute("autocomplete", "off");
-
-    message_input.elt.focus();
-
-    showInputOnRelease = false;
+  if (startMessageOnRelease) {
+    startMessageOnRelease = false;
+    startMessage();
     return;
   }
 }
 
-function moveCamera(x, y, max) {
-  movePoint(camera, { x, y }, max);
+function startMessage() {
+  message_input = createInput("");
+  message_input.id("message-input");
+  message_input.attribute("autocomplete", "off");
+  message_input.attribute("maxlength", "16");
+  message_input.elt.focus();
+  mode = "message";
 }
 
-function movePoint(p1, p2, max = Infinity) {
-  const dX = constrain(p2.x - p1.x, -max, max);
-  const dY = constrain(p2.y - p1.y, -max, max);
-  p1.x += dX;
-  p1.y += dY;
+function sendMessage() {
+  // set message
+  me.message = message_input.value();
+  message_input.remove();
+
+  // start a timer to clear message
+  clearTimeout(messageTimeout);
+  messageTimeout = setTimeout(() => {
+    me.message = undefined;
+  }, 5000);
+
+  mode = "move";
 }
 
-function gfxFromP8(s) {
-  const gfx_s = s.slice(
-    s.indexOf("__gfx__") + 1,
-    s.indexOf("__gfx__") + 1 + 128
-  );
+function cancelMessage() {
+  message_input.remove();
+  mode = "move";
+}
 
-  const colors = [
+////////////////////////////////////////////////////////
+// LOADING
+
+function gfxFromP8(s, colors) {
+  // default palette
+  colors = colors || [
     "#000000", // black
     "#222A54", // navy
     "#7B2654", // plum
@@ -209,6 +222,13 @@ function gfxFromP8(s) {
     "#FBCDA8", // peach
   ];
 
+  // find __gfx__ strings
+  const gfx_s = s.slice(
+    s.indexOf("__gfx__") + 1,
+    s.indexOf("__gfx__") + 1 + 128
+  );
+
+  // draw pixels
   const gfx = createImage(128, 128);
   gfx.loadPixels();
   for (let y = 0; y < 128; y++) {
@@ -218,23 +238,24 @@ function gfxFromP8(s) {
     }
   }
   gfx.updatePixels();
+
   return gfx;
 }
 
-function mapFromP8(s, gfx) {
-  gfx = gfx || gfxFromP8(s);
-
+function mapDataFromP8(s) {
+  // find __map__ strings
   const map_s = s.slice(
     s.indexOf("__map__") + 1,
     s.indexOf("__map__") + 1 + 32
   );
 
+  // find shared portion of __gfx__ strings
   const shared_s = s.slice(
     s.indexOf("__gfx__") + 1 + 64,
     s.indexOf("__gfx__") + 1 + 128
   );
 
-  // shared portion of __gfx__ is different format than __map__ data
+  // shared portion of __gfx__ is in different format than __map__ data
   // need to join pairs of lines
   // need to flip nibble order "abcd" -> "badc"
   function reversePairs(s) {
@@ -252,20 +273,26 @@ function mapFromP8(s, gfx) {
     return out;
   }, []);
 
+  // add shared data to map data
   map_s.push(...map_ext_s);
 
+  return map_s;
+}
+
+function mapFromP8(s, gfx) {
+  gfx = gfx || gfxFromP8(s);
+  const map_s = mapDataFromP8(s);
   const map = createImage(128 * 8, 64 * 8);
-  // map.loadPixels();
+
+  // blit tiles into map
   for (let y = 0; y < 64; y++) {
     for (let x = 0; x < 128; x++) {
       let sprite_x = parseInt(map_s[y][x * 2 + 1], 16);
       let sprite_y = parseInt(map_s[y][x * 2], 16);
-      // shared data bit order is backwards
-      // if (y >= 32) [sprite_x, sprite_y] = [sprite_y, sprite_x];
       if (sprite_x === 0 && sprite_y === 0) continue;
       map.copy(gfx, sprite_x * 8, sprite_y * 8, 8, 8, x * 8, y * 8, 8, 8);
     }
   }
-  // map.updatePixels();
+
   return map;
 }
