ca434358b28c0c6f5837ecb9d685202d11696522
Justin Bakse <justin@tsfim.com>
2022-01-27 22:13:30 -0600

    add todos to no_p5

--- public/examples/no_p5/index.js
+++ public/examples/no_p5/index.js
@@ -16,10 +16,12 @@ async function init() {
   // clients should remove themselves from the room before they disconnect
   // but sometimes they don't or can't
   // clear out any disconnected clients
+  // @todo this should be taken care of for you, even if you are using party directly
   room.removeDisconnectedClients();
 
   console.log("Create Record");
-  // @todo fix next line to use room
+  // @todo fix next line to use room as factory to make record so user doesn't
+  // need to specify full record name
   const record = new party.Record(client, "nop5-main/test");
   await record.whenReady();
   console.log("Record Ready");
@@ -28,18 +30,20 @@ async function init() {
   shared.text = shared.text || "Hello!";
 
   // sync input field
-  // it would be better to do this when the data changes
-  // but right now p5.party.js doesn't expose the subscription events
-  // so we poll here instead.
   const input = document.querySelector("input");
   input.oninput = function () {
     shared.text = input.value;
   };
+  // @todo it would be better to do this when the data changes
+  // but right now p5.party.js doesn't expose the subscription events
+  // so we poll here instead.
   setInterval(() => {
     if (input.value !== shared.text) input.value = shared.text;
   }, 100);
 
   // clean up on exit
+  // @todo, this proably shouldn't be left to user code
+  // p5.party wrapper handles this, maybe this can be handled by party classes directly?
   window.addEventListener("beforeunload", () => {
     room.leave();
     client.close();
