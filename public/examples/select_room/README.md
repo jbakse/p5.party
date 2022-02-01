a25cc5ff1955ecaf9ceed950bdea9a919eee346f
Justin Bakse <justin@tsfim.com>
2022-02-01 17:00:56 -0600

    add select_room example

---
+++ public/examples/select_room/README.md
@@ -0,0 +1,9 @@
+# select_room
+
+This example shows how to allow a user to select a room.
+
+Enter a custom room name into the field, and press "Join Room".
+
+- **click** to move the dot
+
+> Open this example in two browser windows at once!
---
+++ public/examples/select_room/index.js
@@ -0,0 +1,39 @@
+// get the room name from the query string
+const room = new URLSearchParams(location.search).get("room");
+console.log("room:", room);
+
+let shared;
+
+function preload() {
+  partyConnect("wss://deepstream-server-1.herokuapp.com", "hello_party", room);
+  shared = partyLoadShared("shared");
+}
+
+function setup() {
+  createCanvas(400, 400);
+  noStroke();
+
+  if (room) {
+    shared.x = shared.x || 0;
+    shared.y = shared.y || 0;
+  }
+}
+
+function draw() {
+  if (room) {
+    background("#ffcccc");
+    fill("#000066");
+    ellipse(shared.x, shared.y, 100, 100);
+  } else {
+    background("#ffcccc");
+    noStroke();
+    fill("white");
+    textAlign(CENTER);
+    text("Choose a room", 200, 200);
+  }
+}
+
+function mousePressed() {
+  shared.x = mouseX;
+  shared.y = mouseY;
+}
