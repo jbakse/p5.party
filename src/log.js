cc85d20ca2ccd61eb17712b142dca7018f0d004d
Justin Bakse <justin@tsfim.com>
2022-02-02 11:06:35 -0600

    add log level text to log messages

--- src/log.js
+++ src/log.js
@@ -1,24 +1,24 @@
 export const debug = makeLogger(
   "log",
-  "debug",
+  "party-debug",
   "background-color: #888; color: #fff; padding: 2px 5px; border-radius: 2px"
 );
 
 export const log = makeLogger(
   "log",
-  "party",
+  "party-log",
   "background-color: #88F; color: #00ffff; padding: 2px 5px; border-radius: 2px"
 );
 
 export const warn = makeLogger(
   "log",
-  "party",
+  "party-warn",
   "background-color: #FF0; color: #000; padding: 2px 5px; border-radius: 2px"
 );
 
 export const error = makeLogger(
   "error",
-  "party",
+  "party-error",
   "background-color: #ff0000; color: #ffffff; padding: 2px 5px; border-radius: 2px"
 );
 
