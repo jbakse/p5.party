5562297da346b410a8e3bc48fa894a0adce37b57
Justin Bakse <justin@tsfim.com>
2024-03-19 15:16:05 -0500

    remove unused dependencies

--- esbuild.inject.js
+++
@@ -1,1 +0,0 @@
-export let Buffer = require("buffer").Buffer;
--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,12 @@
 /* global module */
 const config = {
   preset: "ts-jest/presets/js-with-ts",
-  testEnvironment: "jsdom",
+  testEnvironment: "jsdom", // requires jest-environment-jsdom
   collectCoverage: true,
   coverageReporters: ["text", "json-summary"],
 
   transformIgnorePatterns: ["/node_modules/(?!(on-change)/)"],
-  moduleNameMapper: {
-    "\\.(css|less|scss|sss|styl)$": "<rootDir>/node_modules/jest-css-modules",
-  },
+
   testRegex: "((\\.|/*.)(test))\\.[jt]sx?$",
 };
 module.exports = config;
